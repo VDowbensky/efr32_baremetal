d7f2146ce50ce1c33a0de33b2dc0d8614e826cf1
VDowbensky <harbinger@ukr.net>
2021-10-10 07:41:37 +0300

    move intermediate files to another repository

--- bmtest2/radio/inc/modem.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MODEM_H_
-#define _MODEM_H_
-
-#include "em_device.h"
-
-void init_modem(void);
-
-#endif
-
--- bmtest2/radio/inc/nibblebits.h
+++
@@ -1,8 +0,0 @@
-#ifndef _NIBBLEBITS_H_
-#define _NIBBLEBITS_H_
-
-#include <stdint.h>
-
-extern const uint8_t nibblebits[];
-
-#endif
